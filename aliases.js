4a8f052d93e0b39d2fcc0a93a04e5bea35329fe5
Resul Elezi <resul_elezi@protonmail.com>
2026-09-23 18:12:28 +0200

    code customized

--- aliases.js
+++ aliases.js
@@ -1,10 +1,6 @@
-// Define short aliases
-window.byId = (id) => document.getElementById(id);
-window.$ = (selector) => document.querySelector(selector);
-window.$$ = (selector) => document.querySelectorAll(selector);
-window.log = console.log;
+// DOM
+const byId = (id) => document.getElementById(id);
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
-// use case
-// const element = $('#myId');
-// const elements = $$('.myClass');
-// const header = byId('header');
+export { byId, $, $$ };
