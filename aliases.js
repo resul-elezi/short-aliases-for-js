dee0bc47557a39f92df8c53146d8b53759085894
Resul Elezi <resul_elezi@protonmail.com>
2026-09-23 19:18:07 +0200

    added alias for some events

--- aliases.js
+++ aliases.js
@@ -3,4 +3,10 @@ const byId = (id) => document.getElementById(id);
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
-export { byId, $, $$ };
+// Event
+const on = (element, event, handler) =>
+  element.addEventListener(event, handler);
+const off = (element, event, handler) =>
+  element.removeEventListener(event, handler);
+
+export { byId, $, $$, on, off };
