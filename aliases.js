287e2c799e51a941543cb11b7e03e9871e105b19
Resul Elezi <resul_elezi@protonmail.com>
2026-09-24 16:41:52 +0200

    #

--- aliases.js
+++ aliases.js
@@ -1,11 +1,3 @@
-/*
- * Short aliases for JavaScript
- * @version: 0.0.1
- * @author: Resul Elezi
- * @license: Licensed under MIT (github.com/resul-elezi/short-aliases-for-js/blob/main/LICENSE)
- * Copyright 2026 Resul Elezi.
- */
-
 // DOM
 const byId = (id) => document.getElementById(id);
 const $ = (selector) => document.querySelector(selector);
